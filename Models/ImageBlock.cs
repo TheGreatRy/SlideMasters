dce363b5635645624bad6af10ebfb09400bb8134
crazyman222214 <schadejr617@gmail.com>
2026-02-10 09:07:28 -0700

    Adding ImageBlock

---
+++ Models/ImageBlock.cs
@@ -0,0 +1,29 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace SlideMasters_BlazorApp.Models
+{
+    public class ImageBlock
+    {
+        public Image Image { get; set; }
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public int BoardX { get; set; }
+        public int BoardY { get; set; }
+
+        public ImageBlock(Image image)
+        {
+            Image = image;
+            Width = image.Width;
+            Height = image.Height;
+        }
+
+        public void MovePiece(int deltaX, int deltaY)
+        {
+            BoardX += deltaX;
+            BoardY += deltaY;
+        }
+    }
+}
