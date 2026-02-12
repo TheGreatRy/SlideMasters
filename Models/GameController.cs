6495fa8ef2890e9f49d6ac12bc857216e76cf55a
crazyman222214 <schadejr617@gmail.com>
2026-02-12 11:52:19 -0700

    Creating Title Screen Functionality

---
+++ Models/GameController.cs
@@ -0,0 +1,25 @@
+﻿namespace SlideMasters_BlazorApp.Models
+{
+    public enum GameState
+    {
+        Title,
+        InProgress,
+        GameOver
+    }
+    public class GameController
+    {
+        public GameState CurrentState { get; private set; }
+        public GameController()
+        {
+            CurrentState = GameState.Title;
+        }
+        
+        public void ChangeState(GameState newState)
+        {
+            if (CurrentState != newState)
+            {
+                CurrentState = newState;
+            }
+        }
+    }
+}
